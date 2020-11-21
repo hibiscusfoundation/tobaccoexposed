0cfd0cfd8c3f1eba71e486f49156d4ff913ef9e2
rakshitha18 <rkssk2014@gmail.com>
2020-11-22 03:23:29 +0530

    style changes

--- script/image.js
+++ script/image.js
@@ -16,12 +16,12 @@ function addFrame(){
   var context = resEle.getContext("2d");
   let BtnEle = document.querySelector(".Btn");
   BtnEle.addEventListener("click", () => {
-      resEle.width = 200;
-      resEle.height = 300;
+      resEle.width = imgEle2.width;
+      resEle.height = imgEle2.height;
       context.globalAlpha = 1.0;
-      context.drawImage(imgEle2, 0, 70,200,200);
+      context.drawImage(imgEle2, 0, 0,imgEle2.width,imgEle2.height);
       context.globalAlpha = 1.0;
-      context.drawImage(imgEle1, 0, 0,200,270);
+      context.drawImage(imgEle1, 0, 0,imgEle2.width,imgEle2.height);
   });
 
   var link = document.createElement('a');
