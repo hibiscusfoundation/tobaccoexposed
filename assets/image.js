461f519097b5aad16867aaca78b7145de7fb72c4
rakshitha18 <rkssk2014@gmail.com>
2020-12-01 15:34:41 +0530

    few changes

--- assets/image.js
+++ assets/image.js
@@ -16,13 +16,13 @@
   let imgEle2 = document.getElementById("blah");
         let resEle = document.querySelector(".result");
         var context = resEle.getContext("2d");
-  let BtnEle = document.querySelector(".Btn");
-      BtnEle.addEventListener("click", () => {
+//   let BtnEle = document.querySelector(".Btn");
+//       BtnEle.addEventListener("click", () => {
       resEle.width = imgEle2.width;
       resEle.height = imgEle2.height;
       context.drawImage(imgEle2, 0, 0,imgEle2.width,imgEle2.height);
       context.drawImage(imgEle1, 0, 0,imgEle2.width,imgEle2.height);
-  });
+//   });
  
 }
 
